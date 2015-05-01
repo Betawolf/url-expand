765292b322c9f86b337268a91ed2f609e0fdf1e1
Matthew Edwards <m.edwards7@lancaster.ac.uk>
2015-05-01 22:59:31 +0100

    Minor tweaks to frontend names and logging
    I made it so the error file is actually used, and tweaked some
    names. This commit actually marks me giving up after a long and
    involved battle with python's import semantics.
    
    I still can't manage to get a sanely working system which would
    let you install the transforms as a lib and also use the script.
    
    Though I've just thought of a new way.

--- urlmorph.py
+++ urlmorph.py
@@ -4,24 +4,27 @@ import argparse
 import transforms
 
 if __name__ == '__main__':
-  transforms = transforms.all_transforms()
+  transformers = transforms.all_transforms()
 
   parser = argparse.ArgumentParser(description='Transform URLs in-place.')
-  parser.add_argument('transformer', help='The transform to apply, one of {'+','.join(transforms.keys())+'}')
+  parser.add_argument('transformer', help='The transform to apply, one of {'+','.join(transformers.keys())+'}')
   parser.add_argument('infile',nargs='?',help='The input file containing text with shortened URLs. Default is STDIN.',default=sys.stdin,type=argparse.FileType('r'))
   parser.add_argument('outfile',nargs='?',help='Writeable output file. Default is STDOUT. ',default=sys.stdout,type=argparse.FileType('w'))
-  parser.add_argument('--errors', '-e',nargs='?',help='Logfile for warnings. Default is STDERR.',default=sys.stderr,type=argparse.FileType('w'))
+  parser.add_argument('--errors', '-e',nargs='?',help='Logfile for warnings. Default is STDERR.',default=sys.stderr)
   parser.add_argument('--args', '-a', nargs='*', help='Optional arguments to pass to the transform.')
   args = parser.parse_args()
 
-  if args.transformer not in transforms:
+  if args.errors != sys.stderr:
+    logging.basicConfig(filename=args.errors,level=logging.INFO)
+
+  if args.transformer not in transformers:
     logging.error('Do not recognise transform `{}`'.format(args.transformer))
     exit()
 
   if args.args:
-    transformer = transforms[args.transformer](*args.args)
+    transformer = transformers[args.transformer](*args.args)
   else:
-    transformer = transforms[args.transformer]()
+    transformer = transformers[args.transformer]()
 
   for line in args.infile:
     args.outfile.write(transformer.transform_in_place(line))
